8b640023c86f30ebe498c721fd52f70a54f10336
Nikki D'Costa <nikkidcosta@gmail.com>
2023-04-19 08:51:53 -0400

    assignment 1 added

---
+++ Assigment1.py
@@ -0,0 +1,16 @@
+for i in range(num_elements):
+    element = input("Enter element {}: ".format(i+1))
+    elements.append(element)
+
+comp_type = input("Which type of comprehension do you want to create? (List/Dict/Set/Generator): ")
+
+if comp_type.lower() == "list":
+    comp = [element for element in elements]
+elif comp_type.lower() == "dict":
+    comp = {element: len(element) for element in elements}
+elif comp_type.lower() == "set":
+    comp = {element for element in elements}
+elif comp_type.lower() == "generator":
+    comp = (element for element in elements)
+
+print("Comprehension:", comp)
